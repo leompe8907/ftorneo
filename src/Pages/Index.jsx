98175da53ecf54c993705baad9b3b6d4c935261e
leompe8907 <veyqpasa1312@gmail.com>
2025-05-22 00:19:07 -0300

    Se estilo y redirecciono el index

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,5 +1,7 @@
 
 import logo from "../Static/Img/Logo.svg"
+import square from "../Static/Img/Rectangle 144690.png"
+import subtract from "../Static/Img/Subtract.png"
 import '../Static/Style/Index.scss'
 
 export default function Index() {
@@ -7,26 +9,25 @@ export default function Index() {
     <>
       <div className="container-general">
         <div className="container-principal">
-          <div className="empty"></div>
-          <div className="logo">
-            <img src={logo} alt="LOGO EMPRESA"className="logo" />
-          </div>
-          <div className="message">
-            <p className="tmessage">
-              A new way to play the consecrated game of chess. A decentralized platform for everyone who wants to earn money playing the most beloved game in the history of mankind. Scholarships, tournaments, chip system, ratings, integrated rapido or blitz chess mode and much more in the future.A new way to play the consecrated game of chess. A decentralized platform for everyone who wants to earn money playing the most beloved game in the history of mankind. Scholarships, tournaments, chip system, ratings, integrated rapido or blitz chess mode and much more in the future.more in the future. aca
-            </p>
-          </div>
-          <div className="container-interaction">
-            <div className="login">
-              <button className="b-login"> Login </button>
+          <div className="container-interno">
+            <img src={square} alt="square" className="square" />
+            <div className="container-logo">
+              <img src={logo} alt="LOGO EMPRESA"className="logo" />
             </div>
-            <div className="register">
-              <button className="b-register">register</button>
+            <div className="message">
+              <p className="tmessage">
+                A new way to play the consecrated game of chess. A decentralized platform for everyone who wants to earn money playing the most beloved game in the history of mankind. Scholarships, tournaments, chip system, ratings, integrated rapido or blitz chess mode and much more in the future.A new way to play the consecrated game of chess. A decentralized platform for everyone who wants to earn money playing the most beloved game in the history of mankind. Scholarships, tournaments, chip system, ratings, integrated rapido or blitz chess mode and much more in the future.more in the future. aca
+              </p>
             </div>
-            <div className="aux">
-              <div className='t&c'></div>
-              <div className="manual"></div>
-              <div className="faqs"></div>
+            <div className="container-interaction">
+                <button className="login"> <a href="/signin">LOGIN</a>  </button>
+                <button className="register"><a href="/signup">REGISTER</a></button>
+              <div className="aux">
+                <button className="b-TC">TÉRMINOS Y CONDIC</button>
+                <button className="b-MU">MANUAL DE USUARIO</button>
+                <button className="b-FAQ">F.A.Qs</button>
+              </div>
+              <img src={subtract} alt="subtract" className="subtract" />
             </div>
           </div>
         </div>
